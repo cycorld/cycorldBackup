206e30afb6712e4a9460b8bce2e84b2c1ab50fbc
Charles Choi <charleschoi87@gmail.com>
2013-04-20 08:20:52 +0000

    hello cycorld

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cycorld::Application.config.secret_token = 'c9553119ae1a66ec7aaa1be8319aaea766751d8a803ee7143fc9aad9945b1a47ab4024d536a3e4cd51eb3e71e9304ccdcaaf2af1efc9deacde1f312907bc82cf'
